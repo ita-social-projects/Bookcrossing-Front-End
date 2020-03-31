5d3224b28be150a3ce8e13139b05c2d7f4ef1fa9
romadmn <ferencrman@gmail.com>
2020-04-01 02:58:40 +0300

    Add server url and CI/CD test

--- src/app/configs/api-endpoint.constants.ts
+++ src/app/configs/api-endpoint.constants.ts
@@ -1,3 +1,4 @@
-export const baseUrl = "https://localhost:44370/api/";
+export const baseUrl = "https://bookcrossingbackend-dev-as.azurewebsites.net/api/";
+//export const baseUrl = "https://localhost:44370/api/";
 export const requestUrl = baseUrl + "requests";
 export const bookUrl = baseUrl + "books";
