dc991549430b94b6c2e0de75471fd671c4b63a20
romadmn <ferencrman@gmail.com>
2020-04-12 16:39:16 +0300

    Closes #53 Implement localization in project

--- src/app/configs/api-endpoint.constants.ts
+++ src/app/configs/api-endpoint.constants.ts
@@ -2,6 +2,6 @@ import { environment } from 'src/environments/environment';
 
 export const baseUrl = environment.apiUrl + "/api/";
 export const requestUrl = baseUrl + "requests";
-export const bookUrl = baseUrl + "books";
+export const bookUrl = baseUrl + "books/";
 export const authorUrl = baseUrl + "authors";
 export const locationUrl = baseUrl + 'location';
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:44370',
+  apiUrl: 'https://bookcrossingbackend-dev-as.azurewebsites.net',
   mapbox: {
     accessToken: 'pk.eyJ1IjoicGVubnkxMjM0IiwiYSI6ImNrOG9scGNjMzB6Zngzb28yejQ3bm9kMjQifQ.t5SaCnw0vo7PClJDIgbb5w'
   }
