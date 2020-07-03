6662034868abf8a9aefe7464db30cbc9e26885aa
Oles <omelanchuk98@gmail.com>
2020-07-03 20:39:54 +0300

    Add fields validation

--- src/app/shared/components/location-popup/location-popup.component.ts
+++ src/app/shared/components/location-popup/location-popup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { LocationService } from 'src/app/core/services/location/location.service';
 import { ILocation } from 'src/app/core/models/location';
@@ -13,8 +14,19 @@ import { MatDialogRef } from '@angular/material/dialog';
 export class LocationPopupComponent implements OnInit {
   locations: ILocation[] = [];
 
+  locationForm: FormGroup;
+
   constructor(public dialogRef: MatDialogRef<LocationPopupComponent>,
-    private locationService: LocationService) { }
+    private locationService: LocationService,
+    private formBuilder: FormBuilder) {
+      this.locationForm = this.formBuilder.group({
+        location: ['', Validators.required],
+        roomNumber: ['', [Validators.required, Validators.maxLength(7)]]
+      })
+     }
+
+  get location() { return this.locationForm.get('location'); }
+  get roomNumber() { return this.locationForm.get('roomNumber'); }
 
   ngOnInit(): void {
     this.locationService.getLocation().subscribe(
@@ -27,4 +39,8 @@ export class LocationPopupComponent implements OnInit {
     )
   }
 
+  onSubmit(): void{
+    console.log(this.location.invalid)
+    console.log(this.locationForm.value);
+  }
 }
