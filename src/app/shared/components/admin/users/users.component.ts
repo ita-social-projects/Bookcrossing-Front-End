7b03d0c91583ce73906f18e9a4d159b834a42f2e
Oles <omelanchuk98@gmail.com>
2020-08-07 01:14:41 +0300

    Added view button and translated table

--- src/app/shared/components/admin/users/users.component.ts
+++ src/app/shared/components/admin/users/users.component.ts
@@ -1,53 +1,43 @@
-import {
-  Component,
-  OnInit,
-  ViewChild,
-} from "@angular/core";
-import { RefDirective } from "../../../directives/ref.derictive";
-import { CompletePaginationParams } from "../../../../core/models/Pagination/completePaginationParameters";
-import { ActivatedRoute, Params, Router } from "@angular/router";
-import { SortParameters } from "../../../../core/models/Pagination/sortParameters";
-import { NotificationService } from "../../../../core/services/notification/notification.service";
-import { TranslateService } from "@ngx-translate/core";
-import { IUserInfo } from "../../../../core/models/userInfo";
-import { UserService } from "src/app/core/services/user/user.service";
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { RefDirective } from '../../../directives/ref.derictive';
+import { CompletePaginationParams } from '../../../../core/models/Pagination/completePaginationParameters';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { SortParameters } from '../../../../core/models/Pagination/sortParameters';
+import { NotificationService } from '../../../../core/services/notification/notification.service';
+import { TranslateService } from '@ngx-translate/core';
+import { IUserInfo } from '../../../../core/models/userInfo';
+import { UserService } from 'src/app/core/services/user/user.service';
 import {
   AbstractControl,
   FormControl,
   FormGroup,
   Validators,
-} from "@angular/forms";
+} from '@angular/forms';
 
 @Component({
-  selector: "app-users",
-  templateUrl: "./users.component.html",
-  styleUrls: ["./users.component.scss"],
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
   @ViewChild(RefDirective, { static: false }) refDir: RefDirective;
 
   public users: IUserInfo[];
-  public usersDisplayColumns: string[] = [
-    "#",
-    "First name",
-    "Last name",
-    "Email",
-    "Register date",
-  ];
+  public usersDisplayColumns: string[];
   public usersProperties: string[] = [
-    "id",
-    "firstName",
-    "lastName",
-    "email",
-    "registeredDate",
+    'id',
+    'firstName',
+    'lastName',
+    'email',
+    'registeredDate',
   ];
-  private fieldsForSearch = ["firstName", "lastName", "email"];
+  private fieldsForSearch = ['firstName', 'lastName', 'email'];
   public queryParams: CompletePaginationParams = new CompletePaginationParams();
   public totalSize: number;
 
   public searchForm: FormGroup;
   public get searchField(): AbstractControl {
-    return this.searchForm.get("searchField");
+    return this.searchForm.get('searchField');
   }
   private errorTimeout: NodeJS.Timeout;
 
@@ -59,23 +49,25 @@ export class UsersComponent implements OnInit {
     private userService: UserService,
     private notificationService: NotificationService,
     private translate: TranslateService
-  ) {}
+  ) {
+    this.usersDisplayColumns = [
+      '#',
+      translate.instant('components.admin.users.first-name'),
+      translate.instant('components.admin.users.last-name'),
+      'Email',
+      translate.instant('components.admin.users.register-date'),
+    ];
+  }
 
   public ngOnInit(): void {
     this.routeActive.queryParams.subscribe((params: Params) => {
       this.queryParams = this.queryParams.mapFromQuery(params);
       this.queryParams.sort.orderByField = this.queryParams.sort.orderByField
         ? this.queryParams.sort.orderByField
-        : "id";
+        : 'id';
 
       this.buildSearchForm(this.queryParams?.filters[0]?.value);
       this.getUsers(this.queryParams);
-    });
-  }
-
-  private buildSearchForm(searchText: string): void {
-    this.searchForm = new FormGroup({
-      searchField: new FormControl(searchText, Validators.maxLength(40)),
     });
   }
 
@@ -112,34 +104,18 @@ export class UsersComponent implements OnInit {
     this.changeUrl();
   }
 
-  private changeUrl(): void {
-    this.router.navigate(["."], {
-      relativeTo: this.routeActive,
-      queryParams: this.queryParams.getQueryObject(),
-    });
-  }
-
-  private getUsers(params: CompletePaginationParams): void {
-    this.userService.getUsers(params).subscribe({
-      next: (pageData) => {
-        this.users = pageData.page;
-        this.totalSize = pageData.totalCount;
-        this.usersLoaded = true;
-      },
-      error: () => {
-        this.notificationService.error(
-          this.translate.instant("common-errors.error-message"),
-          "X"
-        );
-      },
-    });
+  public onViewButtonClicked(user: IUserInfo) {
+    this.notificationService.info(
+      "This action must redirect admin to user's details page. User id = " +
+        user.id,
+      'X'
+    );
   }
 
   public checkLength(control: AbstractControl) {
     if (!control) return;
 
     const maxLength: number = control.errors?.maxlength?.requiredLength;
-
     if (!maxLength) return;
 
     if (control.value.length > maxLength) {
@@ -158,8 +134,32 @@ export class UsersComponent implements OnInit {
     }
   }
 
-  // editLanguage(language: ILanguage) {
-  //   this.languageService.formLanguage = language;
-  //   this.router.navigate(['admin/language-form']);
-  // }
+  private buildSearchForm(searchText: string): void {
+    this.searchForm = new FormGroup({
+      searchField: new FormControl(searchText, Validators.maxLength(40)),
+    });
+  }
+
+  private changeUrl(): void {
+    this.router.navigate(['.'], {
+      relativeTo: this.routeActive,
+      queryParams: this.queryParams.getQueryObject(),
+    });
+  }
+
+  private getUsers(params: CompletePaginationParams): void {
+    this.userService.getUsers(params).subscribe({
+      next: (pageData) => {
+        this.users = pageData.page;
+        this.totalSize = pageData.totalCount;
+        this.usersLoaded = true;
+      },
+      error: () => {
+        this.notificationService.error(
+          this.translate.instant('common-errors.error-message'),
+          'X'
+        );
+      },
+    });
+  }
 }
