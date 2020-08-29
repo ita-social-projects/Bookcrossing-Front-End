ca1ea96ee15cfe08b0a50886d44eb7fd096d953a
Oles <omelanchuk98@gmail.com>
2020-08-29 23:24:03 +0300

    Fixed unit test for user service

--- src/app/core/services/user/user.service.spec.ts
+++ src/app/core/services/user/user.service.spec.ts
@@ -1,4 +1,7 @@
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { userUrl } from '../../../configs/api-endpoint.constants';
@@ -18,9 +21,7 @@ describe('#UserService.getUserById(id), UserService.editUser(id, IUserPut)', () 
     // Configures testing app module
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [
-        UserService
-      ]
+      providers: [UserService],
     });
 
     // Instantaites HttpClient, HttpTestingController and UserService
@@ -36,23 +37,26 @@ describe('#UserService.getUserById(id), UserService.editUser(id, IUserPut)', () 
   // Test case 1
   it('should send user id and get it GET method', () => {
     const newUser: IUserInfo = {
-         id: 1,
-         firstName: 'Andriy',
-         lastName: 'Oleksiuk',
-         email: 'qwerty@gmail.com',
-         isEmailAllowed: true,
-         userLocation: null,
-         role: null,
-         birthDate: null,
-         registeredDate: null,
-         token: null
+      id: 1,
+      firstName: 'Andriy',
+      lastName: 'Oleksiuk',
+      email: 'qwerty@gmail.com',
+      isEmailAllowed: true,
+      userLocation: null,
+      role: null,
+      birthDate: null,
+      registeredDate: null,
+      token: null,
+      isDeleted: false,
     };
     const id = 1;
 
-    userService.getUserById(id).subscribe(
-      data => expect(data).toEqual(newUser, 'should return the user'),
-      fail
-    );
+    userService
+      .getUserById(id)
+      .subscribe(
+        (data) => expect(data).toEqual(newUser, 'should return the user'),
+        fail
+      );
 
     // getUserById should have made one request to GET user
     const req = httpTestingController.expectOne(userUrl + `/${id}`);
@@ -62,30 +66,29 @@ describe('#UserService.getUserById(id), UserService.editUser(id, IUserPut)', () 
   // Test case 2
   it('should send edited user and their id PUT method', () => {
     const newUser: IUserPut = {
-         id: 1,
-         firstName: 'Andriy',
-         lastName: 'Oleksiuk',
-         email: 'qwerty@gmail.com',
-         password: '1234',
-         isEmailAllowed: true,
-         userLocation: null,
-         roleId: 1,
-         birthDate: null,
-         registeredDate: null,
+      id: 1,
+      firstName: 'Andriy',
+      lastName: 'Oleksiuk',
+      email: 'qwerty@gmail.com',
+      password: '1234',
+      isEmailAllowed: true,
+      userLocation: null,
+      roleId: 1,
+      birthDate: null,
+      registeredDate: null,
     };
     const id = 1;
 
-    userService.editUser(id, newUser).subscribe(
-      data => expect(data).toEqual(Object, 'should send User and return'),
-      fail
-    );
+    userService
+      .editUser(id, newUser)
+      .subscribe(
+        (data) => expect(data).toEqual(Object, 'should send User and return'),
+        fail
+      );
 
     // editUser should have made one request to PUT user
     const req = httpTestingController.expectOne(userUrl + '/' + 1);
     expect(req.request.method).toEqual('PUT');
     expect(req.request.body).toEqual(newUser);
   });
-
-
 });
-
