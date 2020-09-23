4706eda471b55d247d23d15ac15ee09eceda80c1
= <=>
2020-09-23 16:23:04 +0300

    Added Message Service to contacts form to work with user messages

--- src/app/core/models/message.ts
+++ src/app/core/models/message.ts
@@ -1,14 +1,4 @@
-// export interface IMessage {
-//     userId: number;
-//     message: string;
-// }
-
-import { IUserInfo } from './userInfo';
-
 export interface IMessage {
-    id?: number;
-    user?: IUserInfo;
-    summary?: string;
-    text?: string;
-    status?: boolean;
+    userId: number;
+    message: string;
 }
---
+++ src/app/core/models/userMessage.ts
@@ -0,0 +1,10 @@
+export interface IUserMessage {
+    id?: number;
+    userId?: number;
+    userFirstName?: string;
+    userLastName?: string;
+    userEmail?: string;
+    summary?: string;
+    text?: string;
+    status?: boolean;
+}
